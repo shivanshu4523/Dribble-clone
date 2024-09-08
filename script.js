cd340f7c3aa2c6bd9fd9f169140aea52ae739100
Shivanshu Srivastava <shivanshusrivastava440@gmail.com>
2024-09-08 22:54:56 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-// Select the burger button and the navigation menu
 const burger = document.querySelector('.nav-v2-burger');
 const navMenu = document.querySelector('.nav-v2-main');
+var modal = document.getElementById("myModal"); 
+var btn = document.getElementById("openModalBtn"); 
+var span = document.getElementsByClassName("close")[0]; 
 
-// Add event listener to toggle 'active' class
+
 burger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+var modalContent = document.getElementsByClassName("modal-content")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+modal.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+modalContent.onclick = function(event) {
+  event.stopPropagation(); 
+}
